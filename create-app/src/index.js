2bf57c5c978f88d48f0db16d6ca9b05d1e9f7f44
Wayne Chang <cywayne8475@gmail.com>
2021-11-29 17:36:49 +0800

    revised link

--- create-app/src/index.js
+++ create-app/src/index.js
@@ -8,7 +8,7 @@ class LineNF extends React.Component {
         <div id="linewindow">
 		    <div id="welcome-message1">Accton Line Notify</div>
 		    <div id="welcome-message2">點擊下方按鈕註冊推播通知</div>
-            <a href="https://notify-bot.line.me/oauth/authorize?response_type=code&scope=notify&response_mode=form_post&client_id=EvNyANGa6nH8QHAKqk1CIY&redirect_uri=https://wayne8475.github.io/LineNotifyTest/&state=998">
+            <a href="https://notify-bot.line.me/oauth/authorize?response_type=code&scope=notify&response_mode=form_post&client_id=EvNyANGa6nH8QHAKqk1CIY&redirect_uri=https://wayne8475.github.io/Line_Notify/&state=998">
                 <button id="loginbutton">Line Notify帳號綁定</button>
             </a>
 	    </div>
