f84717500acf0e915ea28f4f81d7d6c1ae9ab29a
Wayne Chang <cywayne8475@gmail.com>
2021-11-29 17:32:55 +0800

    added line notify link

---
+++ create-app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class LineNF extends React.Component {
+  render() {
+    return (
+        <div id="linewindow">
+		    <div id="welcome-message1">Accton Line Notify</div>
+		    <div id="welcome-message2">點擊下方按鈕註冊推播通知</div>
+            <a href="https://notify-bot.line.me/oauth/authorize?response_type=code&scope=notify&response_mode=form_post&client_id=EvNyANGa6nH8QHAKqk1CIY&redirect_uri=https://wayne8475.github.io/LineNotifyTest/&state=998">
+                <button id="loginbutton">Line Notify帳號綁定</button>
+            </a>
+	    </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<LineNF />, document.getElementById("root"));
